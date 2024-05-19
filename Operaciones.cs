32c109ee43f6d403fb9de692d17f3a3e0e1203e7
iandresw <rbejarano@unah.hn>
2024-05-18 21:31:04 -0600

    Create Operaciones.cs
    Se creo la clase operaciones con las cuatro operaciones básicas y se agrego una captura de execpcion en la división para si introducen 0

---
+++ Operaciones.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioExcepciones
+{
+    internal class Operaciones
+    {
+        public double Suma(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Resta (double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double Multiplicacion(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public double Divide(double num1, double num2)
+        {
+            if(num1 == 0)
+            {
+                throw new DivideByZeroException("No se puede dividir entre Cero");
+            }
+            return num1 / num2;
+        }
+    }
+}
